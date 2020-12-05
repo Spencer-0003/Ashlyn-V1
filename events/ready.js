fe6130f448b5587478883be8bce57f2990338274
Spencer-0003 <spencerthe0003@gmail.com>
2020-12-05 15:09:08 +0000

    Fixed ready event.

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,10 @@
 const getServerCount = require("@utils/GetServerCount");
 
-module.exports = client => {
+module.exports = async client => {
     if (client.shard.ids[0] === 0) {
         console.clear();
         console.log("Logged into bot account.");
-        client.user.setActivity(`with ${getServerCount(client)} servers`, {
+        client.user.setActivity(`with ${await getServerCount(client)} servers`, {
             type: "PLAYING"
         });
 
