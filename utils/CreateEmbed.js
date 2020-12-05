99abbe566f9903e029089948ccec61c5307717b9
Spencer-0003 <spencerthe0003@gmail.com>
2020-12-05 12:07:26 +0000

    Added CreateEmbed.

---
+++ utils/CreateEmbed.js
@@ -0,0 +1,24 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = (options) => {
+    let embed = new MessageEmbed()
+        .setTitle(options.title || "")
+        .setColor(0x00B0FF || options.colour)
+        .setDescription(options.description || "")
+        .setImage(options.image || "")
+        .setFooter(options.footer || "[Ashlyn]")
+        .setAuthor(options.author || "")
+        .setTimestamp();
+
+    if (options.thumbnail !== false) {
+        embed.setThumbnail(options.thumbnail || "https://i.imgur.com/FoezDWf.png");
+    };
+
+    if (options.message) {
+        options.message.forEach(field => {
+            embed.addField(field.name, field.value, field.inline);
+        });
+    };
+
+    return embed;
+};
