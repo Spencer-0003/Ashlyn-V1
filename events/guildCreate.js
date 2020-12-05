9044a31f998d1640f9f520257fbd75695d666db7
Spencer-0003 <spencerthe0003@gmail.com>
2020-12-05 19:21:38 +0000

    Fixed guildCreate event.

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,6 +1,6 @@
 const createEmbed = require("@utils/CreateEmbed");
 
-module.exports = guild => {
+module.exports = (client, guild) => {
     let embed = createEmbed({
         title: "Ashlyn: Welcome",
         description: `Thanks for adding me to ${guild.name}! To get a list of my commands, use !help`
@@ -10,7 +10,7 @@ module.exports = guild => {
         return guild.systemChannel.send(embed);
     } else {
         let defaultChannel;
-        guild.channels.forEach((channel) => {
+        guild.channels.cache.map(channel => {
             if (channel.type == "text" && channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
                 defaultChannel = channel;
                 return;
