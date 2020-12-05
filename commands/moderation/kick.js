dfdaf23b48e818f48fa1802d9999451118c8bfc7
Spencer-0003 <spencerthe0003@gmail.com>
2020-12-05 12:59:23 +0000

    Added kick command.

---
+++ commands/moderation/kick.js
@@ -0,0 +1,57 @@
+const Command = require("@structures/Command");
+const createEmbed = require("@utils/CreateEmbed");
+
+module.exports = class KickCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: "kick",
+            group: "moderation",
+            memberName: "kick",
+            description: "Kicks the chosen user",
+            userPermissions: ["KICK_MEMBERS"],
+            clientPermissions: ["KICK_MEMBERS"],
+            guildOnly: true,
+            args: [
+                {
+                    key: "user",
+                    prompt: "Who would you like to kick?",
+                    type: "user",
+                    default: ""
+                },
+                {
+                    key: "kickReason",
+                    prompt: "Why do you want to kick them?",
+                    type: "string",
+                    default: ""
+                },
+            ],
+        });
+    };
+
+    async run(message, { user, kickReason }) {
+        let embed = createEmbed({
+            title: "Ashlyn: Moderation",
+            description: `You have been kicked from ${message.guild.name} for "${kickReason}"`,
+            message: [{ name: "Moderator", value: `<@${message.author.id}>` }]
+        });
+
+        if (user === message.author) {
+            let errorEmbed = createEmbed({
+                title: "Ashlyn: Moderation",
+                description: `You can't kick yourself.`,
+            });
+
+            return message.say(errorEmbed);
+        };
+
+        try {
+            await user.send(embed);
+        } catch {
+            message.say("Failed to DM this user.");
+        };
+
+        return message.guild.member(user).kick({
+            reason: kickReason
+        });
+    };
+};
