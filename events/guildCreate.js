4721f27b387f9c750d7fb3328c6142a31eb934ba
Spencer-0003 <spencerthe0003@gmail.com>
2020-12-05 13:58:31 +0000

    GuildCreate now uses SystemChannel if possible.

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,18 +1,22 @@
 const createEmbed = require("@utils/CreateEmbed");
 
 module.exports = guild => {
-    let defaultChannel;
-    guild.channels.forEach((channel) => {
-        if (channel.type == "text" && channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
-            defaultChannel = channel;
-            return;
-        };
-    });
-
     let embed = createEmbed({
         title: "Ashlyn: Welcome",
         description: `Thanks for adding me to ${guild.name}! To get a list of my commands, use !help`
     });
 
-    return defaultChannel.send(embed);
+    if (guild.systemChannel && guild.systemChannel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
+        return guild.systemChannel.send(embed);
+    } else {
+        let defaultChannel;
+        guild.channels.forEach((channel) => {
+            if (channel.type == "text" && channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
+                defaultChannel = channel;
+                return;
+            };
+        });
+
+        return defaultChannel.send(embed);
+    };
 };
