5e979da7d5fb41c5be7138b56b997d14b4de9280
Spencer-0003 <spencerthe0003@gmail.com>
2020-12-06 03:57:51 +0000

    Finished auto role.

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,7 +1,17 @@
-// Will be updated in the future, this is just a placeholder.
+const { mongo_db } = process.env;
+const getCollection = require("@utils/GetCollection");
 
 module.exports = (client, member) => {
     if (member.bot) return;
 
-    console.log(`${member.user.tag} has joined ${member.guild.name}`);
+    getCollection(mongo_db, "Auto Roles", async function(collection, _client) {
+        let guildData = await collection.findOne({ GuildID: member.guild.id });
+
+        if (guildData) {
+            let role = guildData.Role;
+            await member.roles.add(role);
+        };
+
+        return _client.close();
+    });
 };
