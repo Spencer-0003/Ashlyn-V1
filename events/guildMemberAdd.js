8ec04da3302721587b3241dfb9b9619acc0e00d8
Spencer-0003 <spencerthe0003@gmail.com>
2020-12-11 16:18:25 +0000

    Added alt detection.

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,8 +1,34 @@
 const { mongo_db } = process.env;
 const getCollection = require("@utils/GetCollection");
+const createEmbed = require("@utils/CreateEmbed");
 
 module.exports = (client, member) => {
     if (member.bot) return;
+
+    getCollection(mongo_db, "Auto Moderation", async function(collection, _client) {
+        let guildData = await collection.findOne({ GuildID: member.guild.id });
+
+        if (guildData && guildData.NoAlts == "true") {
+            if (member.user.createdAt < "259200000") {
+                let embed = createEmbed({
+                    title: "Ashlyn: Moderation",
+                    description: `You have been flagged as an alt and have been auto banned. DM ${member.guild.owner.tag} to appeal`
+                });
+
+                try {
+                    await member.send.try(embed);
+                } catch {
+                    console.log(`Failed to notify ${member.tag} that they have been flagged as an alt.`);
+                };
+
+                member.ban({
+                    reason: "Suspected alt."
+                });
+            };
+        };
+
+        return _client.close();
+    });
 
     getCollection(mongo_db, "Auto Roles", async function(collection, _client) {
         let guildData = await collection.findOne({ GuildID: member.guild.id });
