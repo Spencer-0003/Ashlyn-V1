17b2bf3a3adf6eaae7dc8f8af354290dcd0967a7
Spencer-0003 <spencerthe0003@gmail.com>
2020-12-12 19:05:23 +0000

    Removed hentai command to comply with top.gg.

--- structures/Client.js
+++ structures/Client.js
@@ -1,11 +1,9 @@
 const { CommandoClient } = require("discord.js-commando");
-const NekosLife = require("nekos.life");
 
 module.exports = class AshlynClient extends CommandoClient {
     constructor(options) {
         super(options);
 
         this.queue = new Map();
-        this.neko = new NekosLife();
     };
 };
