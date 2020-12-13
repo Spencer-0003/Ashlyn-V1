7c2bb8d868893bec70eec5f9ce893725de62d13e
Spencer-0003 <spencerthe0003@gmail.com>
2020-12-13 14:44:58 +0000

    Pause is now translated.

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -1,5 +1,6 @@
 const { Command } = require("discord.js-commando");
 const createEmbed = require("@utils/CreateEmbed");
+const getServerLocale = require("@utils/GetServerLocale");
 
 module.exports = class PauseCommand extends Command {
     constructor(client) {
@@ -12,14 +13,17 @@ module.exports = class PauseCommand extends Command {
     };
 
     run(message) {
+        let translations = getServerLocale(message.guild).COMMANDS.MUSIC;
+        let embedTitle = `Ashlyn: ${translations.TITLE}`;
+
         let queue = this.client.queue;
         let serverQueue = queue.get(message.guild.id);
         let voiceChannel = message.member.voice.channel;
 
         if (!serverQueue) {
             let embed = createEmbed({
-                title: "Ashlyn: Music",
-                description: "No song is currently playing."
+                title: embedTitle,
+                description: translations.SONG_NOT_PLAYING
             });
 
             return message.embed(embed);
@@ -27,8 +31,8 @@ module.exports = class PauseCommand extends Command {
 
         if (voiceChannel !== serverQueue.voiceChannel) {
             let embed = createEmbed({
-                title: "Ashlyn: Music",
-                description: "You are not in the voice channel."
+                title: embedTitle,
+                description: translations.NOT_IN_VOICE
             });
 
             return message.embed(embed);
@@ -36,8 +40,8 @@ module.exports = class PauseCommand extends Command {
 
         if (!serverQueue.playing) {
             let embed = createEmbed({
-                title: "Ashlyn: Music",
-                description: "Music is already paused."
+                title: embedTitle,
+                description: translations.ALREADY_PAUSED
             });
 
             return message.embed(embed);
@@ -47,8 +51,8 @@ module.exports = class PauseCommand extends Command {
         serverQueue.connection.dispatcher.pause();
 
         let embed = createEmbed({
-            title: "Ashlyn: Music",
-            description: "Music has been paused."
+            title: embedTitle,
+            description: translations.SUCCESSFULLY_PAUSED
         });
 
         return message.embed(embed);
--- locales/english.js
+++ locales/english.js
@@ -7,7 +7,10 @@ module.exports = {
             CANT_JOIN: "I don't have permission to join the voice channel.",
             CANT_SPEAK: "I don't have permission to speak in the voice channel.",
             ADDED_TO_QUEUE: "has been added to the queue.",
-            ERROR: "There was an unexpected error connecting to the voice channel, if Discord is not down, please alert Spencer#0003"
+            ERROR: "There was an unexpected error connecting to the voice channel, if Discord is not down, please alert Spencer#0003",
+            SONG_NOT_PLAYING: "No song is currently playing.",
+            ALREADY_PAUSED: "Music is already paused.",
+            SUCCESSFULLY_PAUSED: "Music has been paused."
         },
     }
 };
