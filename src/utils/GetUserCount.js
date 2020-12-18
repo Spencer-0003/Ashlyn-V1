3e246634a6ff72951e1a6d94be691bc3a7d06eab
Spencer-0003 <spencerthe0003@gmail.com>
2020-12-18 20:26:12 +0000

    Fixed member count.

--- src/utils/GetUserCount.js
+++ src/utils/GetUserCount.js
@@ -1,18 +1,12 @@
 module.exports = async client => {
-    // let total = 0;
-    let guilds = await client.shard.broadcastEval("guilds");
-    console.log(guilds);
-    // guilds.forEach(element => {
-    //     console.log("NAME: " + element.name);
-    //     console.log("MEMBERS: " + element.memberCount);
-    // });
-    let req = await client.shard.fetchClientValues("users.cache.size");
+    let total = 0;
+    let guilds = await client.shard.broadcastEval("this.guilds.cache");
 
-    // shard.broadcastEval("this.guilds.map(u => u.id)").then(result => {
-    //     for (var i = 0; i<result.length; i++){
-    //         console.log(result[i]);
-    //     }
-    // });
+    guilds.forEach(element => {
+        element.forEach(element2 => {
+            total = total + element2.memberCount;
+        });
+    });
 
-    return req.reduce((p, n) => p + n, 0);
+    return total;
 };
