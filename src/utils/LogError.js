245a8537f329b7c43a556010ad70258fca5f0386
Spencer-0003 <spencerthe0003@gmail.com>
2020-12-18 20:18:06 +0000

    Fixed error logs.

--- src/utils/LogError.js
+++ src/utils/LogError.js
@@ -4,8 +4,10 @@ const createEmbed = require("@utils/CreateEmbed");
 module.exports = (client, type, err) => {
     let embed = createEmbed({
         title: "Ashlyn: Error",
-        messages: [{ name: type, value: `\`\`\`\n${err}\`\`\`` }]
+        message: [{ name: type, value: `\`\`\`\n${err}\`\`\`` }]
     });
 
-    client.channels.fetch(error_log_channel).say(embed);
+    client.channels.fetch(error_log_channel).then(channel => {
+        channel.send(embed);
+    });
 };
