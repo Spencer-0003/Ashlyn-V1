834b5ff3d5c029823dcf7019829cad278e314d49
Spencer-0003 <spencerthe0003@gmail.com>
2020-12-19 18:09:48 +0000

    Ran voice state update through linter.

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -1,22 +1,22 @@
 // Performance Improving!
 module.exports = (client, oldState, newState) => {
-if (newState.channelID === null && oldState.channelID !== null) {
-  let voiceChannel = oldState.guild.channels.cache.get(oldState.channelID)
-  const botChannel = oldState.guild.me.voice.channel;
+    if (newState.channelID === null && oldState.channelID !== null) {
+        let voiceChannel = oldState.guild.channels.cache.get(oldState.channelID);
+        const botChannel = oldState.guild.me.voice.channel;
 
-  if (voiceChannel === botChannel) {
-  if (voiceChannel.members.size === 1) {
-  let serverQueue = client.queue.get(oldState.guild.id);
-        if (serverQueue) {
-            serverQueue.connection.dispatcher.destroy();
-            voiceChannel.leave();
-            client.queue.delete(oldState.guild.id);
-            //There may be a bye bye message
-            return;
+        if (voiceChannel === botChannel) {
+            if (voiceChannel.members.size === 1) {
+                let serverQueue = client.queue.get(oldState.guild.id);
+                if (serverQueue) {
+                    serverQueue.connection.dispatcher.destroy();
+                    voiceChannel.leave();
+                    client.queue.delete(oldState.guild.id);
+                    // There may be a bye bye message
+                    return;
+                }
+                voiceChannel.leave();
+                client.queue.delete(oldState.guild.id);
+            }
         }
-        voiceChannel.leave();
-        client.queue.delete(oldState.guild.id);
-  }
- }
-}
+    }
 };
