5244d0b019545dc5b0bb2353c728fcf90b5c7e35
Spencer-0003 <spencerthe0003@gmail.com>
2020-12-24 09:10:09 +0000

    Fixed crash pt2.

--- src/utils/LogError.js
+++ src/utils/LogError.js
@@ -1,9 +1,10 @@
+const { bot_name } = process.env;
 const { error_log_channel } = process.env;
 const createEmbed = require("@utils/CreateEmbed");
 
 module.exports = (client, type, err) => {
     let embed = createEmbed({
-        title: `${client.user.username}: Error`,
+        title: `${bot_name}: Error`,
         message: [{ name: type, value: `\`\`\`\n${err}\`\`\`` }]
     });
 
