8e3ccc502717c4a1e9d5ae9fa24585d49437dda3
Spencer-0003 <spencerthe0003@gmail.com>
2020-12-28 05:29:51 +0000

    Ew, const bad.

--- src/utils/CanModifyQueue.js
+++ src/utils/CanModifyQueue.js
@@ -1,6 +1,6 @@
 module.exports = member => {
-    const { channelID } = member.voice;
-    const botChannel = member.guild.voice.channelID;
+    let { channelID } = member.voice;
+    let botChannel = member.guild.voice.channelID;
 
     if (channelID !== botChannel) {
         member.send("You need to join the voice channel first!").catch(console.error);
