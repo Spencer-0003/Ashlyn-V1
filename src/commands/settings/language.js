2d54a398c68d80233c8dee8525a4e0aadb2cf0c2
Spencer-0003 <spencerthe0003@gmail.com>
2020-12-29 07:32:21 +0000

    Optimised some code.

--- src/commands/settings/language.js
+++ src/commands/settings/language.js
@@ -1,5 +1,7 @@
+const { mongo_db } = process.env;
 const Command = require("@structures/Command");
 const createEmbed = require("@utils/CreateEmbed");
+const getCollection = require("@utils/GetCollection");
 
 module.exports = class LanguageCommand extends Command {
     constructor(client) {
@@ -26,6 +28,13 @@ module.exports = class LanguageCommand extends Command {
         if(!message.member.hasPermission("ADMINISTRATOR") && !this.client.isOwner(message.author)) {
             return message.reply(translations.GLOBAL.ADMIN_ONLY);
         };
+
+        getCollection(mongo_db, "Guild Settings", async function(collection, client) {
+            let guildData = await collection.findOne({ GuildID: message.guild.id });
+
+            await collection.updateOne(guildData, { $set: { Language: language } });
+            return client.close();
+        });
 
         message.guild.locale = language;
 
--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -3,11 +3,7 @@ const getCollection = require("@utils/GetCollection");
 
 module.exports = (client, guild) => {
     getCollection(mongo_db, "Guild Settings", async function(collection, _client) {
-        let guildData = await collection.findOne({ GuildID: guild.id });
-
-        if (guildData) {
-            await collection.findOneAndDelete({ GuildID: guild.id });
-        };
+        await collection.findOneAndDelete({ GuildID: guild.id });
 
         return _client.close();
     });
