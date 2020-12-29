859d182cea880dc74716d2bb2f74b6f476d7ffd7
Spencer-0003 <spencerthe0003@gmail.com>
2020-12-29 04:30:36 +0000

    Disabled boost DMs in Discord Bots.

--- src/events/guildMemberBoost.js
+++ src/events/guildMemberBoost.js
@@ -1,6 +1,9 @@
 const createEmbed = require("@utils/CreateEmbed");
 
 module.exports = (client, member) => {
+    // requested by moderator
+    if (member.guild.id === "110373943822540800") return;
+
     let embed = createEmbed({
         title: `${client.user.username}: Boost`,
         description: `Thanks for boosting ${member.guild.name}!`
