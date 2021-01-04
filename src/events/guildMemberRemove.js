8d1aacda8992794f14f95136a3867aad2523fa8e
Spencer-0003 <spencerthe0003@gmail.com>
2021-01-04 13:18:44 +0000

    XP is now reset if you leave the guild.

--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -1,7 +1,7 @@
-// Will be updated in the future, this is just a placeholder.
+const Levels = require("discord-xp");
 
 module.exports = (client, member) => {
     if (member.bot) return;
 
-    console.log(`${member.user.tag} has left ${member.guild.name}`);
+    Levels.deleteUser(member.id, member.guild.id);
 };
