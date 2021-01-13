062645e2e8114329c9c123b4de8532cc236832e5
Spencer-0003 <spencerthe0003@gmail.com>
2021-01-13 16:56:59 +0000

    Rewrote some code.

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -1,19 +1,12 @@
-// Performance Improving!
 module.exports = (client, oldState, newState) => {
     if (newState.channelID === null && oldState.channelID !== null) {
         let voiceChannel = oldState.guild.channels.cache.get(oldState.channelID);
         let botChannel = oldState.guild.me.voice.channel;
 
-        if (voiceChannel === botChannel) {
-            if (voiceChannel.members.size === 1) {
-                let serverQueue = client.queue.get(oldState.guild.id);
-                if (serverQueue) {
-                    serverQueue.connection.dispatcher.destroy();
-                    voiceChannel.leave();
-                    client.queue.delete(oldState.guild.id);
-                    // serverQueue.textChannel.send("I left the channel because nobody left.");
-                    return;
-                };
+        if (voiceChannel === botChannel && voiceChannel.members.size === 1) {
+            let serverQueue = client.queue.get(oldState.guild.id);
+            if (serverQueue) {
+                serverQueue.connection.dispatcher.destroy();
                 voiceChannel.leave();
                 client.queue.delete(oldState.guild.id);
             };
